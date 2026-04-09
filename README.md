f03fbf1735f9344d671195cd80b56a32ff6d0a71
Liberty1031 <2681035285@qq.com>
2026-04-09 23:29:23 +0800

    question

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 5.综上：JDK=JRE+开发工具集
         JRE=JVM+Java SE标准类库
         JDK=JVM+Java SE标准类库+开发工具集。
-6.
+6.public class Hello中，Hello表示是一个 类 ，public static void main(String.[] args)表示一个类的入口。
+7.顺序：Hello.java---（编译javac）---Hello.class---（运行）java---JVM for Windows
+javac是运行Java语言的编译器;.java文件是我能看懂的形式，.class文件是javac编译过后的语言；JVM是.class文件的专属运行环境。
--- hello.java
+++ hello.java
@@ -1,11 +1,5 @@
 public class Hello{
-    //Hello是public公有的一个类
     public static void main(String[] args){
-        //Hello{}表示一个类的开始与结束
-        // public static void main(String[] args)表示一个主方法，即程序的入口
-        //main(){}表示方法的开始与结束
-        
-        System.out.println("Hello,world~");
+        System.out.println("索晗 is studying java!");
     }
-
 }
